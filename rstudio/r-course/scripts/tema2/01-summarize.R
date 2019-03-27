cf5a68ce0e8345b54e0e822fe5f967d8e3be7639
danidepo <sdanielpm@gmail.com>
2019-03-27 20:03:50 +0100

    tema2

--- rstudio/r-course/scripts/tema2/01-summarize.R
+++ rstudio/r-course/scripts/tema2/01-summarize.R
@@ -2,44 +2,17 @@ data <- read.csv("../data/tema2/auto-mpg.csv",
                  header = TRUE,
                  stringsAsFactors = FALSE)
 
+## Si es un valor peque�o y se repite mucho es mejor transformarlo a factores
 data$cylinders <- factor(data$cylinders, 
-                         levels = c(3,4,5,6,8),
+                         level = c(3,4,5,6,8), 
                          labels = c("3cil", "4cil", "5cil", "6cil", "8cil"))
-
 
 summary(data)
 
 str(data)
 
-
 summary(data$cylinders)
+
 summary(data$mpg)
+
 str(data$cylinders)
-
-
-install.packages(c("modeest", "raster", "moments"))
-library(modeest) #moda
-library(raster) #quantiles, cv
-library(moments) # asimetría, curtosis
-
-
-X = data$mpg
-
-#### Medidas de Centralización
-mean(X) #sum(X)/length(X)
-median(X)
-mfv(X)
-quantile(X)
-
-#### Medidas de Dispersión
-var(X)
-sd(X)
-cv(X) #sd(X)/mean(X)
-
-#### Medidas de asimetría
-skewness(X)
-moments::kurtosis(X)
-
-par(mfrow = c(1,1))
-hist(X)
-
