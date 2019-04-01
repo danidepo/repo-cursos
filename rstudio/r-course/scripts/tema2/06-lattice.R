840d62678182a596d54bd92a4bdf3d561edbf841
Daniel del Pozo <sdanielpm@gmail.com>
2019-04-01 16:19:09 +0200

    graficos

---
+++ rstudio/r-course/scripts/tema2/06-lattice.R
@@ -0,0 +1,19 @@
+auto <- read.csv("../data/tema2/auto-mpg.csv", stringsAsFactors = F)
+auto$cylinders = factor(auto$cylinders,
+                        levels = c(3,4,5,6,8),
+                        labels = c("3C", "4C", "5C", "6C", "8C"))
+library(lattice)
+
+## ~ eje vertical ~ eje horizontal
+bwplot(~ auto$mpg | auto$cylinders,
+       main = "MPG según cilindrada",
+       xlab = "Millas por Galeón",
+       layout = c(2,3), aspect = 1)
+
+xyplot(mpg~weight | cylinders, data = auto,
+       main = "Peso vs Consumo vs Cilindrada",
+       xlab = "Peso (kg)", ylab = "Consumo (mpg)")
+
+##bowplot, xyplot, densityplot, splom
+
+trellis.par.set(theme = col.whitebg())
--- rstudio/r-course/scripts/tema3/01-confussion-matrices.R
+++ rstudio/r-course/scripts/tema3/01-confussion-matrices.R
@@ -1,24 +1,20 @@
 cp <- read.csv("../data/tema3/college-perf.csv")
 
-cp$Perf <- ordered(cp$Perf, 
-                   levels = c("Low", "Medium", "High"))
-cp$Pred <- ordered(cp$Pred,
-                   levels = c("Low", "Medium", "High"))
+cp$Perf <- ordered(cp$Perf, levels = c("Low","Medium","High"))
+
+cp$Pred <- ordered(cp$Pred, levels = c("Low","Medium","High")) 
 
 
-table <- table(cp$Perf, cp$Pred, 
-               dnn =  c("Actual", "Predecido"))
+#Matriz de confusión, da los datos de acierto y de fallo
+table <- table(cp$Perf, cp$Pred, dnn = c("Actual", "Predecido"))
+
 table
 
 prop.table(table)
 
-round(prop.table(table, 1)*100, 2)
+#Por filas
+round(prop.table(table,1)*100,2)
 
-round(prop.table(table, 2)*100, 2)
+#Por columnas
+round(prop.table(table,2)*100,2)
 
-barplot(table, legend = TRUE, 
-        xlab = "Nota predecida por el modelo")
-
-mosaicplot(table, main = "Eficiencia del modelo")
-
-summary(table)
