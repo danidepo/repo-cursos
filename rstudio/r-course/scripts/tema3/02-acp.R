f188c5136a4f625860a5d23db846f7b89f0bed13
Daniel del Pozo <sdanielpm@gmail.com>
2019-04-01 17:27:09 +0200

    Update 02-acp.R

--- rstudio/r-course/scripts/tema3/02-acp.R
+++ rstudio/r-course/scripts/tema3/02-acp.R
@@ -1,3 +1,5 @@
+##Analisis componentes principales
+
 usarrests <- read.csv("../data/tema3/USArrests.csv", stringsAsFactors = FALSE)
 
 rownames(usarrests) <- usarrests$X
@@ -6,5 +8,24 @@ rownames(usarrests) <- usarrests$X
 usarrests$X <- NULL
 
 head(usarrests)
+
+# Varianza
 apply(usarrests, 2, var)
- 
+
+acp <- prcomp(usarrests, 
+             center = TRUE, scale = TRUE)
+print(acp)
+
+plot(acp, type = "l")
+
+summary(acp)
+
+biplot(acp, scale = 0)
+
+pc1 <- apply(acp$rotation[,1]*usarrests, 1, sum)
+pc2 <- apply(acp$rotation[,2]*usarrests, 1, sum)
+pc2
+
+usarrests$pc1 <- pc1
+usarrests$pc2 <- pc2
+usarrests[,1:4] = NULL
