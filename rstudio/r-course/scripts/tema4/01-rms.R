edb9644bf2dba16fb953dc508f43dc474740be97
Daniel del Pozo <sdanielpm@gmail.com>
2019-04-03 16:27:34 +0200

    tema4

--- rstudio/r-course/scripts/tema4/01-rms.R
+++ rstudio/r-course/scripts/tema4/01-rms.R
@@ -1,13 +1,17 @@
 dat <- read.csv("../data/tema4/rmse.csv")
 
-rmse<-sqrt(mean((dat$price - dat$pred)^2))
+#Nos preguntamos si los datos predichos son buenos
+
+#Error cuadratico, nos dice cuanto nos equivocamos en funcion del valor original
+rmse <- sqrt(mean((dat$price - dat$pred)^2))
 rmse
 
 plot(dat$price, dat$pred, xlab = "Actual", ylab="Predicho")
+
+#Cuanto mas cerca un punto este de la recta menos error tiene
 abline(0,1)
 
-
-rmse <- function(actual, predicted){
+rmse <- function(actual, predicted) {
   return(sqrt(mean((actual-predicted)^2)))
 }
 
--- rstudio/r-course/scripts/tema4/03-linear-regression.R
+++ rstudio/r-course/scripts/tema4/03-linear-regression.R
@@ -1,18 +1,27 @@
+#Utilizamos el paquete caret para la particion de datos
 library(caret)
+
 auto <- read.csv("../data/tema4/auto-mpg.csv")
+
 auto$cylinders <- factor(auto$cylinders,
                          levels = c(3,4,5,6,8),
-                         labels = c("3c", "4c", "5c", "6c", "8c"))
-
+                         labels = c("3c","4c","5c","6c","8c"))
 set.seed(2018)
+
 t.id <- createDataPartition(auto$mpg, p = 0.7, list = F)
+
+#Vemos que columnas excluimos en el analisis
 names(auto)
 
-mod <- lm(mpg ~ ., data = auto[t.id,-c(1,8,9)])
+#Creamos el modelo lineal pasandole los identificadores de entrenamiento
+#No necesitamos crear variables dummies, el lm lo hace automaticamente para las variables
+#categoricas
+
+mod <- lm(mpg ~., data = auto[t.id, -c(1,8,9)])
 
 mod
 
-#mpg = 38.607312 +
+#Podemos predecir el consumo, a partir de obtener un valor de mpg = 38.607312 +
 #     + 7.212652*4c + 5.610350*5c + 3.307172*6c + 6.211343*8c +
 #     + 0.006878 * displacement - 0.072209 * horsepower +
 #     - 0.005156 * weight + 0.024852 * acceleration
@@ -24,25 +33,8 @@ boxplot(mod$residuals)
 sqrt(mean((mod$fitted.values - auto[t.id,]$mpg)^2))
 
 pred <- predict(mod, auto[-t.id, -c(1,8,9)])
+
 sqrt(mean((pred - auto[-t.id,]$mpg)^2))
 
 par(mfrow=c(2,2))
 plot(mod)
-
-
-auto <- within(auto, 
-               cylinders <- relevel(cylinders, ref="4c"))
-mod <- lm(mpg ~. , data = auto[t.id, -c(1,8,9)])
-summary(mod)
-pred <- predict(mod, auto[-t.id, -c(1,8,9)])
-sqrt(mean((pred-auto[-t.id,]$mpg)^2))
-plot(mod)
-
-library(MASS)
-
-mod
-summary(mod)
-
-step.model <- stepAIC(mod, direction="backward")
-summary(step.model)
-
