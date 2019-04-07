86ff1c4c892454ba234b9a68e38b328680dc3784
danidepo <sdanielpm@gmail.com>
2019-04-07 12:08:22 +0200

    Update 01-clustering-hierarchical.R

--- rstudio/r-course/scripts/tema5/01-clustering-hierarchical.R
+++ rstudio/r-course/scripts/tema5/01-clustering-hierarchical.R
@@ -13,8 +13,36 @@ hc
 #Visualizamos el Dendogram (Clustering jerarquico)
 plot(hc, hang = -0.01, cex = 0.7)
 
-hc2 <- hclust(dist(data, method = "euclidean"),
-              method = "single")
+hc2 <- hclust(dist(data, method = "euclidean"),   #manhattan
+              method = "single") #minimo
 
 plot(hc2, hang = -0.01, cex = 0.7)
+
+hc3 <- hclust(dist(data, method = "euclidean"),   #manhattan
+              method = "complete") #maximo
+
+plot(hc3, hang = -0.01, cex = 0.7)
+
+hc3$merge
+
+hc4 <- hclust(dist(data, method = "euclidean"),   #manhattan
+              method = "average") #promedio
+
+plot(hc4, hang = -0.01, cex = 0.7)
+
+hc4$merge
+
+d<- dist(data, method = "euclidean")
+
+#Distancia euclidean comparan dos elementos como de lejos estan una de otra
+d
+
+alb <- data["Albania",-10]
+aus <- data["Austria",-10]
+alb-aus
+sqrt(sum((alb-aus)^2))
+sum(abs(alb-aus))
+
+
+
 
